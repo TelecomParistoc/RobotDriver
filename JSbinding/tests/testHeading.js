16df398e721d184eb357f0a488f510b80e190b8f
Arnaud <arnaudbonetti@me.com>
2015-11-29 13:54:29 +0100

    fixed tests : added missing initMotorDriver

--- JSbinding/tests/testHeading.js
+++ JSbinding/tests/testHeading.js
@@ -1,5 +1,6 @@
 var driver = require('../motordriver.js');
 
+console.log("initializing driver : " + driver.initMotorDriver());
 setInterval(function() {
     console.log("heading : "+ driver.heading() + "°");
 }, 200);
