38ebdced51227556ff41cacf6f7d9c711a63bd55
Arnaud <arnaudbonetti@me.com>
2015-12-30 20:20:23 +0100

    create queue system for motion controller

---
+++ src/queue.c
@@ -0,0 +1,38 @@
+#include "queue.h"
+#include <stdlib.h>
+
+struct queueElement {
+    void* content;
+    struct queueElement* nextElement;
+}
+
+static queueElement* queueHead = NULL;
+static queueElement* queueTail = NULL;
+
+void addToQueue(void* elementPtr) {
+    struct queueElement* newElement = malloc(sizeof(struct queueElement));
+    newElement->content = elementPtr;
+    newElement->nextElement = NULL;
+    if(queueTail != NULL)
+        queueTail->nextElement = newElement;
+    if(queueHead == NULL)
+        queueHead = newElement;
+}
+
+void* getHead() {
+    if(queueHead!=NULL)
+        return queueHead->content;
+    return NULL;
+}
+void removeHead() {
+    if(queueHead != NULL) {
+        free(queueHead->content);
+        free(queueHead);
+        queueHead = queueHead->nextElement;
+    }
+}
+
+void clearQueue() {
+    while(queueHead != NULL)
+        removeHead();
+}
