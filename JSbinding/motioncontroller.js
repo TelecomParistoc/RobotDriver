63a813fd405a09e36cc7462c571db7718e85a1b4
Arnaud <arnaudbonetti@me.com>
2016-01-02 20:26:10 +0100

    created JS binding for motion controller

---
+++ JSbinding/motioncontroller.js
@@ -0,0 +1,31 @@
+var ffi = require('ffi');
+var ref = require('ref');
+
+var voidPtr = ref.refType('void');
+var simpleCallback = ffi.Function('void', []);
+var motionCallback = ffi.Function('void', [voidPtr]);
+
+var lib = ffi.Library('./../build/librobotdriver', {
+    // motioncontroller
+    'initMotionController': [ 'int', [] ],
+    'getMaxAcceleration': [ 'double', [] ],
+    'setMaxAcceleration': [ 'void', ['double'] ],
+    'setBlockingCallback': [ 'void', [simpleCallback] ],
+    // speedcontroller :
+    'queueSpeedChange': [ 'void', ['double', motionCallback]],
+    'queueSpeedChangeAt': [ 'void', ['int', 'double', motionCallback]],
+    'queueStopAt': [ 'void', ['int', motionCallback] ],
+    'clearMotionQueue': ['void', []],
+    'getDistanceTolerance': [ 'unsigned int', [] ],
+    'setDistanceTolerance': [ 'void', ['unsigned int'] ],
+    // headingcontroller :
+    'getTargetHeading': [ 'double', [] ],
+    'setTargetHeading': [ 'void', ['double', simpleCallback] ],
+    'turnOf': [ 'void', ['double', simpleCallback] ],
+    'getCurrentHeading': [ 'double', [] ],
+    'getHeadingTolerance': [ 'double', [] ],
+    'setHeadingTolerance': [ 'void', ['double'] ],
+    'enableHeadingControl' : ['void', ['int'] ]
+});
+
+module.exports = lib;
