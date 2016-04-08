cb7882717e9163c7a2d753ea9e48ace7143119d1
Arnaud <arnaudbonetti@me.com>
2016-04-08 16:07:06 +0200

    added comment

--- src/motioncontroller.h
+++ src/motioncontroller.h
@@ -33,6 +33,8 @@ double getMaxAcceleration();
  * by default, it is called about every 200ms if still blocked
  * (see getMotorDriverUpdateFreq() in motordriver.h) */
 void setBlockingCallback(void (*callback)(void));
+
+/* works the same way as setBlockingCallback but usage is reserved to pathFollower */
 void setRecalibrationCallback(void (*callback)(void));
 
 #ifdef __cplusplus
