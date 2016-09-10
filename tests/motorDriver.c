11280a1143d7ffbbbd3068e993aa554d854eb621
superpingu <arnaudbonetti@me.com>
2016-09-10 07:32:07 +0200

    wip

--- tests/motorDriver.c
+++ tests/motorDriver.c
@@ -5,7 +5,12 @@
 
 int main(int argc, char * argv[])
 {
-  int res = I2Cread16(MB_ADDR, 0x00);
-  printf(">%d-%d<", (res & 0xFF00) >> 8, res & 0x00FF);
+  int res = I2Cread16(MB_ADDR, 0x01);
+  printf(">%d-%d<\n", (res & 0xFF00) >> 8, res & 0x00FF);
+  I2Cwrite16(MB_ADDR, 0x00, 0x0123);
+  res = I2Cread16(MB_ADDR, 0x05);
+  printf(">%d-%d<\n", (res & 0xFF00) >> 8, res & 0x00FF);
+  res = I2Cread16(MB_ADDR, 0x00);
+  printf(">%d-%d<\n", (res & 0xFF00) >> 8, res & 0x00FF);
   return 0;
 }
