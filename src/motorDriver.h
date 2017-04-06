597fcde5f5dc53361ab9176a3830f8b05f7ad5fb
Vincent Gaillard <vincent.gaillard@telecom-paristech.fr>
2017-04-07 00:28:39 +0200

    header

---
+++ src/motorDriver.h
@@ -0,0 +1,29 @@
+int  getPosX();
+int  getPosY();
+int  getHeading();
+void setHeading(int heading);
+
+int  getWheelRightDist();
+void setWheelRightDist(int dist);
+int  getWheelLeftDist();
+void setWheelLeftDist(int dist);
+
+int  getMaxAcc();
+void setMaxAcc(int acc);
+int  getMaxAngularAcc();
+void setMaxAngularAcc(int acc);
+int  getCruiseSpeed();
+void setCruiseSpeed(int speed);
+int  getCruiseAngularSpeed();
+void setCruiseAngularSpeed(int speed);
+int  getGoalMeanDist();
+void setGoalMeanDist(int dist);
+int  getGoalHeading();
+void setGoalHeading(int heading);
+int  getHeadingDistSyncRef();
+void getHeadingDistSyncRef(int dist);
+
+int  getLinearPID();
+void setLinearPID(int linPid);
+int  getAngularPID();
+void setAngularPID(int angPid);
