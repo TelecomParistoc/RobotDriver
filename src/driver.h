c088caff5a07a9484d146e00eb0a1774cbd3fef9
Vincent Gaillard <vincent.gaillard@telecom-paristech.fr>
2017-04-07 00:11:47 +0200

    copy WalkinDriver repository

---
+++ src/driver.h
@@ -0,0 +1,7 @@
+/*
+ * This header provides an access to all the components of walkingdriver,
+ * allowing the programmer to include this only file
+ */
+
+#include <walkingdriver/ax12driver.h>
+#include <walkingdriver/timing.h>
