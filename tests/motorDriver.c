37d4763c0c041d86f2de8d4960212bc9567cc376
superpingu <arnaudbonetti@me.com>
2017-05-01 14:31:35 +0200

    tests heading

--- tests/motorDriver.c
+++ tests/motorDriver.c
@@ -16,7 +16,8 @@ int main(int argc, char * argv[])
   printf("position [%d,%d], heading >%d°<\n", x, y, heading/10);
   move_t dest = {x + 20, y + 100, heading + 900, RADIUS};
   printf("new position [%d,%d], new heading >%d°<\n", dest.x, dest.y, dest.angle/10);
-  goTo(RADIUS, &dest);
+  //goTo(RADIUS, &dest);
+  setHeading(dest.angle);
   printf("position [%d,%d], heading >%d°<\n", getPosX(), getPosY(), getHeading()/10);
 	return 0;
 }
